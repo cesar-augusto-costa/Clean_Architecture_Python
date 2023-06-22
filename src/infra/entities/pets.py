342b9ad57cb23a7dd836e04ec94bc43ec6045f6d
Cesar Costa <cesar.costa.cac.1993@gmail.com>
2023-06-22 13:36:11 -0300

    feat: implementing select_user in UserRepository

--- src/infra/entities/pets.py
+++ src/infra/entities/pets.py
@@ -27,3 +27,14 @@ class Pets(Base):
 
     def __repr__(self):
         return f"Pet: [name={self.name}, specie={self.spacie}, user_id={self.user_id}]"
+
+    def __eq__(self, other):
+        if (
+            self.id == other.id
+            and self.name == other.name
+            and self.spacie == other.spacie
+            and self.age == other.age
+            and self.user_id == other.user_id
+        ):
+            return True
+        return False
